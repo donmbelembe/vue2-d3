ac1a7fdaa1dd28722f8a4d509c9c4664cc114aed
donmbelembe <donmbelembe@gmail.com>
2017-09-04 09:56:29 +0530

    export plugin

--- vue-d3.js
+++ vue-d3.js
@@ -8,5 +8,16 @@
         throw new Error('[vue-d3] cannot locate d3');
       }
 
-      console.log(D3)
+      VueD3.install = function (Vue) {
+        Vue.prototype.$d3 = D3;
+      };
+
+      if (typeof exports == 'object') {
+        module.exports = VueD3;
+      } else if (typeof define == 'function' && define.amd) {
+        define([], function(){ return VueD3; })
+      } else if (window.Vue) {
+        window.VueD3 = VueD3;
+        Vue.use(VueD3);
+      }
 })();
