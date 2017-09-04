ba3e59017eaefe6e09b39d32ba88dbbdd8b7320e
donmbelembe <donmbelembe@gmail.com>
2017-09-04 09:52:09 +0530

    require d3.js or throw error

--- vue-d3.js
+++ vue-d3.js
@@ -1,3 +1,12 @@
 (function  () {
-    console.log('test')
+    var VueD3 = {};
+    var D3 = typeof require === 'function'
+      ? require('d3')
+      : window.D3;
+
+      if (!D3) {
+        throw new Error('[vue-d3] cannot locate d3');
+      }
+
+      console.log(D3)
 })();
